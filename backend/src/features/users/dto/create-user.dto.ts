57f4d779e65a88fdda39160cd959dfe9af444b05
angel0n <63112226+angel0n@users.noreply.github.com>
2026-03-26 17:10:36 -0300

    Backend add start feature user

--- backend/src/features/users/dto/create-user.dto.ts
+++ backend/src/features/users/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import * as Joi from 'joi';
+
+export class CreateUserDto {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const createUserSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).required(),
+});
--- backend/src/features/users/users.controller.ts
+++ backend/src/features/users/users.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, createUserSchema } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JoiValidationPipe } from '../../pipes/JoiValidationPipe';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body(new JoiValidationPipe(createUserSchema)) createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
